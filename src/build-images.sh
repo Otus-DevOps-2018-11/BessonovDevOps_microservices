24cccd4b7c3eb3b8f320b20b6a013248a33d171b
Daniil <bessonov.devops@gmail.com>
2019-04-12 14:17:05 +0300

    ready to deploy reddit containers

---
+++ src/build-images.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker pull mongo:latest
+docker build -t bessonovd/post:1.0 ./post-py
+docker build -t bessonovd/comment:1.0 ./comment
+docker build -t bessonovd/ui:1.0 ./ui
---
+++ src/stop-app.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#docker network create reddit
+docker stop post_db
+docker stop post
+docker stop comment
+docker stop ui
